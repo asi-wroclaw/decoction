64d85476e286f719f2590311578e0d9676f8f50f
Michał Zając <michal.zajac@gmail.com>
2017-01-05 23:31:05 +0100

    Initial User model

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule Decoction.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 3.0"},
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
