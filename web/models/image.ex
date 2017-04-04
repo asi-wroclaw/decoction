7c208037de1caf36baac60a2ad661144b276aad2
Michał Zając <michal.zajac@gmail.com>
2017-04-04 22:27:07 +0200

    Minor fixes to keep the code clean

--- web/models/image.ex
+++ web/models/image.ex
@@ -13,13 +13,11 @@ defmodule Decoction.Image do
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:text, :image, :source_link, :tags, :nsfw])
     |> cast_attachments(params, [:image])
     |> validate_required([:image])
   end
+
 end
